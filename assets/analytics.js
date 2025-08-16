0aa50bb18f5623fd9e5d6ee249174f0c7bd77d20
n-imai <8196611+n-imai@users.noreply.github.com>
2025-08-16 21:02:53 +0900

    feat(analytics): add shared GA4 loader and include on all pages

---
+++ assets/analytics.js
@@ -0,0 +1,19 @@
+/* GA4 loader: Set your measurement ID below */
+(function () {
+  var MEASUREMENT_ID = 'G-XXXXXXXXXX';
+  if (!MEASUREMENT_ID || /X{2,}/.test(MEASUREMENT_ID)) {
+    return;
+  }
+  var s = document.createElement('script');
+  s.async = true;
+  s.src = 'https://www.googletagmanager.com/gtag/js?id=' + MEASUREMENT_ID;
+  document.head.appendChild(s);
+
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){ dataLayer.push(arguments); }
+  window.gtag = gtag;
+  gtag('js', new Date());
+  gtag('config', MEASUREMENT_ID);
+})();
+
+
